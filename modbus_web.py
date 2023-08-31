4974cfe7051a22d4ab4739ec130fe66fb53a81bd
bard <xx61509xx@gmail.com>
2023-08-31 17:55:47 +0800

    🚧 WIP: 前端表單完成 後端銜接中

--- modbus_web.py
+++ modbus_web.py
@@ -1,5 +1,7 @@
+from threading import Thread, Event
 import time
 
+from engineio.async_drivers import threading # * 替代解決辦法 socketio使用threading 打包才能執行
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
 from pymodbus.client import ModbusTcpClient
@@ -8,12 +10,10 @@ from pymodbus.payload import BinaryPayloadDecoder
 
 # request.sid 連線房間
 
-
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode="threading")
 
-# 指定要发送数据的IP地址
-target_ip = "192.168.1.100"  # 替换为你的目标IP地址
+modbus_connect_room = {} # sid: obj
 
 @app.route('/')
 def index():
@@ -34,28 +34,75 @@ def handle_connect():
 
 @socketio.on('connect_modbus')
 def connect_modbus(ip, port):
-    print(ip, port, request.sid,  '-----------')
-    # TODO 建立並執行連線
-
-    # client = ModbusTcpClient(ip, port=int(port))
-    # connection = client.connect()
-    # if not connection:
-    #     print(f"無法建立連線")
-
-
+    modbusThread = ModbusThread(ip, port, request.sid)
+    modbusThread.start()
+    modbus_connect_room[request.sid] = modbusThread
+    print(modbus_connect_room)
     # emit('status_response', {'data': data}, room=request.sid)
 
 @socketio.on('disconnect_modbus')
 def disconnect_modbus():
-    print(request.sid,  '-----------')
-    # TODO 斷開連線 刪除線程
-
+    if request.sid in modbus_connect_room:
+        modbus_connect_room[request.sid].exit_signal.set()
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    print('Client disconnected', request.sid)
-    # TODO 斷開連線 刪除線程
+    if request.sid in modbus_connect_room:
+        modbus_connect_room[request.sid].exit_signal.set()
 
+@socketio.on('modbus_point_data')
+def handle_point_data(data):
+    modbus_connect_room[request.sid].point_data = data
+
+class ModbusThread(Thread):
+    def __init__(self, ip, port, room):
+        super(ModbusThread, self).__init__()
+        self.ip = ip
+        self.port = port
+        self.room = room
+        self.exit_signal = Event()
+        self.point_data = {'point_type': '3', 'slave': '1', 'point_data': {'1': {'log': 0, 'title': '', 'address': '3000', 'data_type': '1', 'bit_num': '64'}}}
+
+    def run(self):
+        client = ModbusTcpClient(self.ip, port=int(self.port))
+        connection = client.connect()
+
+        try:
+            if not connection:
+                with app.app_context():
+                    emit('connect_modbus_error', {'data': 'modbus 無法建立連線'}, namespace='/', broadcast=True, room=self.room)
+            else:
+                with app.app_context():
+                    emit('connect_modbus_success', {'data': 'modbus 連線成功'}, namespace='/', broadcast=True, room=self.room)
+
+                point_type = self.point_data.get('point_type', '3')
+                slave = self.point_data.get('slave', '1')
+                if point_type == '1':
+                    read_funt = client.read_coils
+                elif point_type == '2':
+                    read_funt = client.read_discrete_inputs
+                elif point_type == '4':
+                    read_funt = client.read_input_registers
+                else:
+                    read_funt = client.read_holding_registers
+
+
+                while not self.exit_signal.is_set():
+                    point_data = self.point_data.get('point_data', {})
+                    if len(point_data):
+
+                        result = read_funt(1, count=10, slave=int(slave))
+                        # TODO 目前進度
+
+
+
+                    time.sleep(1)
+
+        finally:
+            # print('斷開連線', self.room)
+            del modbus_connect_room[self.room]
+            print("剩餘連線數:", len(modbus_connect_room))
+            
 
 
 
